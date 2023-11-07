b370e826207d794a4660e8c60adbb433c825dc22
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2023-11-07 14:21:12 +0300

    functional layout

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/components/Layout";
 
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Songs from "./pages/Songs";
 
 import "./App.css";
 import "./styles/Pages.css"
@@ -14,6 +16,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="songs" element={<Songs />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ app/src/pages/Songs.jsx
@@ -0,0 +1,10 @@
+
+function Songs () {
+    return (
+        <>
+            <h1>yeah</h1>
+        </>
+    )
+}
+
+export default Songs
