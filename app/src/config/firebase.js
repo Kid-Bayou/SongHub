694a2e686208d043fab95c8bae106e10fcc3df4c
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2023-11-14 15:37:24 +0300

    .

--- app/src/config/firebase.js
+++ app/src/config/firebase.js
@@ -1,17 +1,20 @@
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyByGkC6tPsxRUw9DdSVdZdb73WHunFsMBY",
-  authDomain: "songhub-48b99.firebaseapp.com",
-  projectId: "songhub-48b99",
-  storageBucket: "songhub-48b99.appspot.com",
-  messagingSenderId: "308408026258",
-  appId: "1:308408026258:web:1d2a58159392828376124b",
-  measurementId: "G-SZ8HZLCKRR",
+  apiKey: "AIzaSyBbXLANXL6CdkghNQe44PRSuClPSRQTDAQ",
+  authDomain: "songhub-46ca7.firebaseapp.com",
+  projectId: "songhub-46ca7",
+  storageBucket: "songhub-46ca7.appspot.com",
+  messagingSenderId: "37367776365",
+  appId: "1:37367776365:web:23acd1d41d5afa3a9beb64",
+  measurementId: "G-2CHPFSD0SG"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
--- app/src/pages/Songs.jsx
+++ app/src/pages/Songs.jsx
@@ -1,27 +1,71 @@
 import { css } from "@emotion/react";
 
-const button = css `
- background-color: #422226;
- color: #F6EDEF;
- font-size: 1rem;
- cursor: pointer;
- border: solid #422226 2px;
- border-radius: 20px;
- width: 100px;
- height: 50px;
+import { db, storage } from "../config/firebase";
+import { useEffect, useState } from "react";
+import { getDocs, collection, doc } from "firebase/firestore";
+import { ref, uploadBytes } from "firebase/storage";
 
- &:hover {
-    background-color: #D1A6AC;
- }
+import noImage from "../assets/noImage.jpg"
+
+const button = css`
+  background-color: #422226;
+  color: #f6edef;
+  font-size: 1rem;
+  cursor: pointer;
+  border: solid #422226 2px;
+  border-radius: 20px;
+  width: 100px;
+  height: 50px;
+
+  &:hover {
+    background-color: #d1a6ac;
+  }
+`;
+
+const songs_container = css `
+display: flex;
+width
+`
+
+const card = css `
+
 `
 
 function Songs() {
+  const [songs, setSongs] = useState([]);
+
+  const songsCollectionRef = collection(db, "songs");
+
+  const getSongs = async () => {
+    try {
+      const data = await getDocs(songsCollectionRef);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setSongs(filteredData);
+    } catch {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getSongs();
+  })
 
   return (
     <>
 
+      <div css={songs_container}>
+        {songs.map((song) => (
+          <div key={song.id}>
+            <h3>{song.name}</h3>
+            <h4>{song.artist}</h4>
+          </div>
+        ))}
+      </div>
+
       <button css={button}>Add Song</button>
-      
     </>
   );
 }
